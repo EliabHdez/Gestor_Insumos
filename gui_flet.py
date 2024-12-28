a65f0e0ba2bdf8f929f4e4c040cceee91eca39b2
Eliab Hdez <moises_hl_zod@hotmail.com>
2024-12-28 13:04:24 -0600

    Cambios en el codigo del archivo gui_flet.py

--- gui_flet.py
+++ gui_flet.py
@@ -1,13 +1,11 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.horizontal_alignment = "center"
-    page.title = 'Gestor de Insumos'
-    page.add(
-        ft.Text("Gestor Entrada / Salida de Insumos", size=30)
-        )
-    page.add(
-        ft.Text("Seleccione la opcion deseada")
-    )
+    page.horizontal_alignment = "CENTER"
+    page.title = "Gestor de Insumos"
+    titulo = ft.Text("Gestor Entrada / Salida de Insumos", size=50, bgcolor="#222222", color="white")
+    # titulo.bgcolor = 'red'
+    # titulo.size = 50
+    page.add(titulo)
 
 ft.app(main)
